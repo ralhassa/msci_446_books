38063cee376dc0a3c0d13b041a3b788c52af1676
hmasha <hmasha@uwaterloo.ca>
2018-11-13 00:12:19 -0500

    Goodreads_10k-list has explanatory and class variable

--- Explanatory_Class.R
+++ Explanatory_Class.R
@@ -1,5 +1,5 @@
-books_2_film = read.csv("C:/MAMP/htdocs/msci_446_books/Book-to-Film_Complete.csv", header=T)
-all_books = read.csv("C:/MAMP/htdocs/msci_446_books/Book_Dataset_Task2_Filtered.csv", header=T)
+books_2_film = read.csv("C:/MAMP/htdocs/msci_446_books/Goodreads_Books_with_Film_Adaptions.csv", header=T)
+all_books = read.csv("C:/MAMP/htdocs/msci_446_books/Goodreads_10k-list.csv", header=T)
 View(books_2_film)
 View(all_books)
 
@@ -10,27 +10,29 @@ numcol2 = ncol(books_2_film)
 
 colnames(books_2_film)<-c(1:numcol2)
 
-class_col = matrix(0,nrow = numrow1, ncol = 1)
-class_col2= matrix(1,nrow = numrow2, ncol = 1)
-exp_class = cbind(all_books,class_col)
+class_col = matrix("no",nrow = numrow1, ncol = 1)
+class_col2= matrix("yes",nrow = numrow2, ncol = 1)
+exp_class = cbind(all_books,class_col, stringsAsFactors = FALSE)
+
 book_film_class=cbind(books_2_film,class_col2)
+
 colnames(exp_class)<-c(1:numcol1)
-output = merge(exp_class,book_film_class,by.x=4, by.y=1)
-View(output)
+
+
    #View(bookfilm)
-   for(row2 in 1:numrow2){ #iterate through all books
+   for(row2 in 1:numrow2){ #iterate through books with film
      #bookall = all_books[row1,4]
     # toString(tolower(bookall))
      #for(row2 in 1: numrow2){ #iterate through books with film
-       bookfilm = books_2_film[row2,1]
-       toString(tolower(bookfilm))
+       bookfilm = books_2_film[row2,3] #store book id in variable
+      # toString(tolower(bookfilm))
    #View(exp_class[,4])
    if (!is.na(bookfilm)){
      
-     if(bookfilm %in% exp_class[,4]){
-       row_match = match(bookfilm, tolower(exp_class[,4]))
+     if(bookfilm %in% exp_class[,1]){
+       row_match = match(bookfilm, tolower(exp_class[,1])) #get index of match
      #  if(identical(bookall,bookfilm)){
-         exp_class[row_match,numcol1+1]= 1
+         exp_class[row_match,numcol1+1]= "yes"
       # }
    }
    #}
@@ -39,4 +41,6 @@ View(output)
  }
    
  View(exp_class)
- View(match(1, exp_class[,4]))
+ install.packages("xlsx")
+ library("xlsx")
+ write.xlsx(x= as.data.frame(exp_class), "C:/Users/hilla/OneDrive/Documents/mydata.xlsx")
