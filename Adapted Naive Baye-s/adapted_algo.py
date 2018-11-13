7146282b6849798be80fd3c607be1067f7ebd013
ralhassa <43611479+ralhassa@users.noreply.github.com>
2018-11-13 02:11:12 -0500

    This is the code
    This is the code to preform the naive baye's algorithm on the adapted (book to movie) data

---
+++ Adapted Naive Baye-s/adapted_algo.py
@@ -0,0 +1,165 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.naive_bayes import MultinomialNB
+
+
+dataset = pd.read_csv('adapted_data.csv')
+print(dataset)
+
+# prepare datasets to be fed into the naive bayes model
+#predict attend class given extra hours and grade
+
+#yes/no column
+CV =  dataset.adapted.values.reshape((len(dataset.adapted), ))
+data = dataset.loc[:,'average_rating':'number_of_ratings'].values.reshape((len(dataset.adapted), 2))
+
+# Create model object
+NB = GaussianNB()
+
+# Train the model using the training sets
+NB.fit(data, CV)
+
+#Model
+print("Probability of the classes: ", NB.class_prior_)
+print("Mean of each feature per class:\n", NB.theta_)
+print("Variance of each feature per class:\n", NB.sigma_)
+
+#predict the class for each data point
+predicted = NB.predict(data)
+print("Predictions:\n",np.array([predicted]).T)
+
+
+# predict the probability/likelihood of the prediction
+prob_of_pred = NB.predict_proba(data)
+print("Probability of each class for the prediction: \n",prob_of_pred)
+
+
+print("Accuracy of the model: ",NB.score(data,CV))
+
+print("The confusion matrix:\n", metrics.confusion_matrix(CV, predicted, ['yes','no']))
+
+
+# Calculating 5 fold cross validation results
+model = GaussianNB()
+kf = KFold(n_splits=5)
+scores = cross_val_score(model, data, CV, cv=kf)
+print("MSE of every fold in 5 fold cross validation: ", abs(scores))
+print("Mean of the 5 fold cross-validation: %0.2f" % abs(scores.mean()))
+print("Does a Book with a 3 as its average rating, from 23 reviews get adapted to a movie: ", 
+      NB.predict(np.array([3,23]).reshape(1, -1)),
+      NB.predict_proba(np.array([3,23]).reshape(1, -1)))
+
+
+# In[9]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+h = .02  # step size in the mesh
+
+names = ["Naive Bayes"]
+classifiers = [GaussianNB()]
+
+
+X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
+                           random_state=1, n_clusters_per_class=1)
+
+
+rng = np.random.RandomState(2)
+X += 2 * rng.uniform(size=X.shape)
+linearly_separable = (X, y)
+
+datasets = [make_moons(noise=0.3, random_state=0),
+            make_circles(noise=0.2, factor=0.5, random_state=1),
+            linearly_separable]
+
+
+# In[10]:
+
+
+figure = plt.figure(figsize=(9, 9))
+i = 1
+# iterate over datasets
+for ds_cnt, ds in enumerate(datasets):
+    # preprocess dataset, split into training and test part
+    X, y = ds
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test =         train_test_split(X, y, test_size=.4, random_state=42)
+
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # just plot the dataset first
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    if ds_cnt == 0:
+        ax.set_title("Input data")
+    # Plot the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+               edgecolors='k')
+    # and testing points
+    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.4,
+               edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xticks(())
+    ax.set_yticks(())
+    i += 1
+
+    # iterate over classifiers
+    for name, clf in zip(names, classifiers):
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        clf.fit(X_train, y_train)
+        score = clf.score(X_train, y_train)
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+#         if hasattr(clf, "decision_function"):
+#         Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+#         else:
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=cm, alpha=0.9)
+
+        # Plot also the training points
+        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                   edgecolors='k')
+        # and testing points
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   edgecolors='k', alpha=0.6)
+
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        if ds_cnt == 0:
+            ax.set_title(name)
+        ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+        i += 1
+
+plt.tight_layout()
+plt.show()
+
