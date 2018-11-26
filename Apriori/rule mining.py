bb19020edd5aef6fc717c44e365163e4a855b327
Melanie Wang <msmangochan@gmail.com>
2018-11-26 11:14:14 -0500

    deleting big folders

--- Apriori/rule mining.py
+++ Apriori/rule mining.py
@@ -17,13 +17,3 @@ print(len(association_results))
 count = len(association_results)
 for i in range(0, count):
     print(association_results[i])
-
-# store_data = pd.read_csv('store_data.csv', header=None)
-# records = []
-# for i in range(0, 7501):
-#     records.append([str(store_data.values[i, j]) for j in range(0, 20)])
-#
-# association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
-# association_results = list(association_rules)
-# print(len(association_results))
-# print(association_results[0])
