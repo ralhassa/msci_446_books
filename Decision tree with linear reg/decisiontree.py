2b5090ac9ed805aeea38caee1ba8e3a61a0d3a6c
hmasha <hmasha@uwaterloo.ca>
2018-11-27 13:31:46 -0500

    updated decision tree

--- Decision tree with linear reg/decisiontree.py
+++ Decision tree with linear reg/decisiontree.py
@@ -17,7 +17,7 @@ print(dataset.describe())
 X = dataset.drop('Class', axis=1)
 y = dataset['Class']
 from sklearn.model_selection import train_test_split  
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
 from sklearn.tree import DecisionTreeRegressor  
 regressor = DecisionTreeRegressor()  
 regressor.fit(X_train, y_train) 
