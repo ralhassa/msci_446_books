05142ebf16822b00bf362cad9280ef88a4d02ca4
Melanie Wang <msmangochan@gmail.com>
2018-11-26 22:21:33 -0500

    going back no authors

--- Apriori/rule mining.py
+++ Apriori/rule mining.py
@@ -3,13 +3,16 @@
 import pandas as pd
 from apyori import apriori
 
-data = pd.read_csv('Dataset_apriori_yes.csv', encoding="utf8")
+# data = pd.read_csv('Dataset_apriori_yes.csv', encoding="utf8")
+data = pd.read_csv('Dataset_apriori_no.csv', encoding="utf8")
+
 
 records = []
-for i in range(0, 1139):
-    records.append([str(data.values[i, j]) for j in range(0, 7)])
+# for i in range(0, 1139):
+for i in range(0, 8861):
+    records.append([str(data.values[i, j]) for j in range(0, 4)])
 
-association_rules = apriori(records, min_support=0.01, min_confidence=0.5,  min_lift=1.5, min_length=2)
+association_rules = apriori(records, min_support=0.001, min_confidence=0.5,  min_lift=1.5, min_length=2)
 association_results = list(association_rules)
 print(len(association_results))
 # count = len(association_results)
